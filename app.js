806430f5a20720742d4cb28c408ea5a3010968c4
sabrina <sabrinaabrodi20@gmail.com>
2022-06-29 12:40:26 +0200

    selecting required emelemnts

---
+++ app.js
@@ -0,0 +1,4 @@
+const wrapper = document.querySelector(".wrapper"),
+  qrInput = wrapper.querySelector(".form input"),
+  generateBtn = wrapper.querySelector(".form button"),
+  qrImg = wrapper.querySelector(".qr-code img");
