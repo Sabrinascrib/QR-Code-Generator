7abb3afffc1ed2fc4812254a62d6521d1d629d3d
sabrina <sabrinaabrodi20@gmail.com>
2022-06-29 18:49:11 +0200

    adding class name active when click

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ const wrapper = document.querySelector(".wrapper"),
   qrInput = wrapper.querySelector(".form input"),
   generateBtn = wrapper.querySelector(".form button"),
   qrImg = wrapper.querySelector(".qr-code img");
+
+generateBtn.addEvenetListener("click", () => {
+  wrapper.classList.add("active");
+});
