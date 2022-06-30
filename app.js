f47fbb201f75a9df79702829e69ef3352f1f6ee7
sabrina <sabrinaabrodi20@gmail.com>
2022-06-30 15:34:36 +0200

    adding fuctionality to the click

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const wrapper = document.querySelector(".wrapper"),
   generateBtn = wrapper.querySelector(".form button"),
   qrImg = wrapper.querySelector(".qr-code img");
 
-generateBtn.addEvenetListener("click", () => {
-  wrapper.classList.add("active");
+generateBtn.addEventListener("click", () => {
+  let qrValue = qrInput.value;
+  if (!qrValue) return;
+  generateBtn.innerText = "Generating QR Code...";
+  qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
+  qrImg.addEventListener("load", () => {
+    wrapper.classList.add("active");
+    generateBtn.innerText = "Generate QR Code";
+  });
 });
